f93db7322a07a5ec98347827031a1b9da3657135
baba-s <shotababa1206@gmail.com>
2022-09-23 13:07:56 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Kogane.TypeExtensionMethods
+# Kogane Type Extension Methods
+
+Type の拡張メソッド
+
+## 使用例
+
+```csharp
+Debug.Log( typeof( int ).IsNullable() );  // False
+Debug.Log( typeof( int? ).IsNullable() ); // True
+```
---
+++ Runtime/TypeExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kogane
+{
+    public static class TypeExtensionMethods
+    {
+        public static bool IsNullable( this Type self )
+        {
+            return Nullable.GetUnderlyingType( self ) != null;
+        }
+    }
+}
